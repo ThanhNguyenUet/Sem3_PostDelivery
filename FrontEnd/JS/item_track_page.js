dac65020c754f424f8ac6041fc1087c186b74c64
Kwan <nhimcoi262@gmail.com>
2023-12-17 11:04:57 +0700

    Done item track page

--- FrontEnd/JS/item_track_page.js
+++ FrontEnd/JS/item_track_page.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const inputField = document.querySelector('input[name="id"]');
+
+    inputField.addEventListener('focus', function () {
+        setTimeout(function () {
+            inputField.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+        }, 100);
+    });
+});
+
 function redirect() {
     window.location.href = "login.html"
 }
